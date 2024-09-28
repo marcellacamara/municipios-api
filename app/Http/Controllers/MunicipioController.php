f70a5509830b56bc3886c838a6e681deb93d4884
marcellacamara <marcella-camara@live.com>
2024-09-28 17:48:21 -0300

    refactor: :recycle: Utiliza constante de UFs válidas

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Services\MunicipioService;
+use Symfony\Component\HttpFoundation\Response;
 
 class MunicipioController extends Controller
 {
     protected $municipioService;
+
+    // Constante com a lista de UFs válidas
+    const UFS_VALIDAS = [
+        'AC',
+        'AL',
+        'AP',
+        'AM',
+        'BA',
+        'CE',
+        'DF',
+        'ES',
+        'GO',
+        'MA',
+        'MT',
+        'MS',
+        'MG',
+        'PA',
+        'PB',
+        'PR',
+        'PE',
+        'PI',
+        'RJ',
+        'RN',
+        'RS',
+        'RO',
+        'RR',
+        'SC',
+        'SP',
+        'SE',
+        'TO'
+    ];
 
     /**
      * @param MunicipioService $municipioService Instância do serviço de municípios.
@@ -26,6 +57,11 @@ class MunicipioController extends Controller
      */
     public function listar($uf)
     {
+        // Verifica se a UF passada é válida
+        if (!in_array(strtoupper($uf), self::UFS_VALIDAS)) {
+            return response()->json(['error' => 'UF inválida'], Response::HTTP_NOT_FOUND);
+        }
+
         $municipios = $this->municipioService->listarMunicipios($uf);
 
         return response()->json($municipios);
